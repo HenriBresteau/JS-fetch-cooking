bb74356699714031fdeb3d0848f16ded1ed77f14
henriB <henri.bretseau@gmail.com>
2020-11-10 12:18:42 +0100

    search Display

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const searchInput = document.getElementById('searchInput');
-const results= document.getElementById('results');
+const results = document.getElementById('results');
 const randomMeal = document.getElementById('randomMeal');
 
-const fetchSearch = async() => {
+let search = ''
+
+const fetchSearch = async () => {
     meals = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=tomato`
-    ).then(res=> res.json())
-    .then(res => res.meals)
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    ).then(res => res.json())
+        .then(res => res.meals)
     console.log(meals);
 }
 
-fetchSearch();
+const searchDisplay = async () => {
+    await fetchSearch();
+    if (meals == null) {
+        results.innerHTML = '<span class="noResult">Aucun résultat</span>';
+    }
+    results.innerHTML = (
+        meals.map(meal => (
+            `
+            <div class="searchContainer">
+                <h2>${meal.strMeal}</h2>
+                <div class="infos">
+                    <div>origine : ${meal.strArea} </div>
+                    <div>categorie : ${meal.strCategory} </div>
+                </div>
+                <img src="${meal.strMealThumb}" alt=""></br>
+                <a href="${meal.strYoutube}" target="_blanck"> <i class="fab fa-youtube"></i></a>
+            </div>
+    `
+        )).join('')
+    );
+}
+
+searchInput.addEventListener('input', (e) => {
+    search = e.target.value
+    searchDisplay();
+})
